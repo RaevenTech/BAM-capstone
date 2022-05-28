4dccd7ebcc0cd00b89c2c6bdbefefc0594d520f5
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-28 18:19:30 +0100

    no bugs

--- bam/src/staticComponents/main/MainLayout.js
+++ bam/src/staticComponents/main/MainLayout.js
@@ -1,13 +1,22 @@
 import React from "react";
+import styles from "./MainLayout.module.css";
 import FooterLayout from "../footer/FooterLayout";
 import NavbarLayout from "../navbar/NavbarLayout";
 
 const MainLayout = () => {
     return (
-        <div>
-            <NavbarLayout />
-            <FooterLayout />
-        </div>
+        <section>
+            <header className="fluid">
+                <div className={styles.navbar}>
+                    <NavbarLayout />
+                </div>
+            </header>
+            <footer>
+                <div>
+                    <FooterLayout />
+                </div>
+            </footer>
+        </section>
     );
 };
 
