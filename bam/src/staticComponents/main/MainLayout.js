998a3457b2dbbd9920c0ece1b1edb3f75477c00f
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-28 18:00:03 +0100

    basic functional components added

---
+++ bam/src/staticComponents/main/MainLayout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FooterLayout from "../footer/FooterLayout";
+import NavbarLayout from "../navbar/NavbarLayout";
+
+const MainLayout = () => {
+    return (
+        <div>
+            <NavbarLayout />
+            <FooterLayout />
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ bam/src/staticComponents/navbar/NavbarLayout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavbarLayout() {
+    return (
+        <div>
+            <h1>Navbar Layout</h1>
+        </div>
+    );
+}
+
+export default NavbarLayout;
