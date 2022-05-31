7f2814343aa3bdba136f62ac1abc7b239d2155f1
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-31 11:26:16 +0100

    hero section with search

---
+++ bam/src/staticComponents/hero/HeroSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HeroSection = () => {
+    return (
+        <section>
+            <img src="" alt="Gallery" />
+            <h1>Like it? Bid for it!</h1>
+            <h3>Online holdiay auction</h3>
+            <p>
+                Find your next vacation, weekend away or travel for business.{" "}
+            </p>
+        </section>
+    );
+};
+
+export default HeroSection;
