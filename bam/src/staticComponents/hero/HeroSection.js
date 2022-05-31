941b448a401fd00e4053e2148be725133c302ac2
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-31 12:49:34 +0100

    hero section incomplete
    no bugs

--- bam/src/staticComponents/hero/HeroSection.js
+++ bam/src/staticComponents/hero/HeroSection.js
@@ -7,17 +7,16 @@ const HeroSection = () => {
         <section id="hero">
             <div className={styles.background}>
                 <img
-                    src="https://wallpaperaccess.com/full/2456957.jpg"
+                    src="https://thumbs.dreamstime.com/b/collage-travel-photos-different-countries-collage-international-landmarks-popular-travel-destinations-photos-108973833.jpg"
                     alt="Gallery"
                 />
             </div>
             <div className={styles.content}>
                 <div className={styles.title}>
                     <h1>Like it? Bid for it!</h1>
-                    <h3>Online holdiay auction</h3>
                     <p>
-                        Find your next vacation, weekend away or travel for
-                        business.{" "}
+                        Find your next vacation destination. Look fora weekend
+                        away or maybe just for business.{" "}
                     </p>
                 </div>
                 <FilterSearch />
