1e16a1dbb9599e7db43d2164a7857c8e278c18b8
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-06-02 11:36:45 +0100

    new listing cards added

--- bam/src/features/cards/NewListings.js
+++ bam/src/features/cards/NewListings.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { Badge } from "react-bootstrap";
+import styles from "./NewListings.module.css";
 
 const NewListings = () => {
-    return <div>NewListings</div>;
+    const data = [
+        {
+            image: "https://www.mensjournal.com/wp-content/uploads/2021/01/MeadowlarkTreehouse.jpg?quality=40&strip=all",
+            location: "Montana,USA",
+            currentbid: 50,
+        },
+        {
+            image: "https://media.cntraveler.com/photos/5e18e330ac1cea00092e91d2/master/pass/airbnb-beach-dominican-6939168.jpeg",
+            location: "Bavaro, Dominican Republic",
+            currentbid: 20,
+        },
+        {
+            image: "https://a0.muscache.com/im/pictures/c5fce685-fff6-4c14-901a-e35a89353a6e.jpg?im_w=720",
+            location: "Camps Bay Beach, Cape Town",
+            currentbid: 50,
+        },
+        {
+            image: "https://a0.muscache.com/im/pictures/d500cce2-6ff0-445f-bd29-1fc5f974c684.jpg?im_w=720",
+            location: "Faro,Portugal",
+            currentbid: 50,
+        },
+        {
+            image: "https://wander-lush.org/wp-content/uploads/2021/01/Best-Airbnbs-in-Italy-Chapel-Venice.jpg",
+            location: "Venice, Italy",
+            currentbid: 50,
+        },
+    ];
+
+    return (
+        <section id="new_Listing">
+            {data.map((card, i) => {
+                return (
+                    <div key={[i]} className={styles.card}>
+                        <img
+                            className={styles.cardImage}
+                            src={card.image}
+                            alt="treehouse"
+                        />
+                        <h4 className={styles.title}>{card.location}</h4>
+                        <Badge className={styles.badge} variant="warning">
+                            €{card.currentbid}
+                        </Badge>
+                        <button className={styles.bid_btn} variant="success">
+                            Bid
+                        </button>
+                    </div>
+                );
+            })}
+            ;
+        </section>
+    );
 };
 
 export default NewListings;
--- bam/src/staticComponents/main/MainLayout.js
+++ bam/src/staticComponents/main/MainLayout.js
@@ -4,6 +4,7 @@ import FooterLayout from "../footer/FooterLayout";
 import NavbarLayout from "../navbar/NavbarLayout";
 import HeroSection from "../hero/HeroSection";
 import HomePageCard from "../../features/cards/HomePageCard";
+import NewListings from "../../features/cards/NewListings";
 
 const MainLayout = () => {
     return (
@@ -20,12 +21,18 @@ const MainLayout = () => {
                 <div className="container-fluid pl-0">
                     <div className="row">
                         <div className="col-8">
+                            <h2>Active Auctions</h2>
+                            <hr />
                             <div className={styles.mainCards}>
                                 <HomePageCard />
                             </div>
                         </div>
                         <div className="col-4">
-                            <div className={styles.newList}>new listing</div>
+                            <h2>New listing</h2>
+                            <hr />
+                            <div className={styles.newList}>
+                                <NewListings />
+                            </div>
                         </div>
                     </div>
                 </div>
