b23bb80df8750cb8c7f55d871d7b9069ed61aa7b
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-06-08 18:24:50 +0100

    seller pager header

--- bam/src/userComponets/seller/SellerPage.js
+++ bam/src/userComponets/seller/SellerPage.js
@@ -1,9 +1,26 @@
 import React from "react";
 import styles from "./SellerPage.module.css";
 import data from "../../data/data.json";
+import NavbarUser from "../user/userNav/NavbarUser";
+import FooterLayout from "../../homeComponents/footer/FooterLayout";
 
 const SellerPage = () => {
-    return <div className={styles.seller_main}></div>;
+    return (
+        <>
+            <NavbarUser />
+            <div className={styles.dashboard}>
+                <h2 className={styles.header}> Your Dashboard </h2>
+                <div>
+                    <ul className={styles.ul}>
+                        <li className={styles.li}>Home</li>
+                        <li className={styles.li}>Log out</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={styles.seller_main}></div>;
+            <FooterLayout />
+        </>
+    );
 };
 
 export default SellerPage;
