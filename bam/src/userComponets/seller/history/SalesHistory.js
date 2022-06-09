2b17566c921a87dc97ee590141cd7a31fe4a6070
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-06-09 12:50:06 +0100

    seller history

--- bam/src/userComponets/seller/history/SalesHistory.js
+++ bam/src/userComponets/seller/history/SalesHistory.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./SalesHistory.module.css";
 
 const SalesHistory = () => {
-    return <div>SalesHistory</div>;
+    return (
+        <div className={styles.auction_history}>
+            <h3>SalesHistory</h3>
+        </div>
+    );
 };
 
 export default SalesHistory;
