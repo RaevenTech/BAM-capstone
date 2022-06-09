3d8b084ca5718f2a7e24351d8b0cc992b579b159
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-06-09 12:23:14 +0100

    main section for seller done

--- bam/src/userComponets/seller/SellerPage.js
+++ bam/src/userComponets/seller/SellerPage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./SellerPage.module.css";
 import data from "../../data/data.json";
 import NavbarUser from "../user/userNav/NavbarUser";
 import FooterLayout from "../../homeComponents/footer/FooterLayout";
+import SalesHistory from "./history/SalesHistory";
 
 const SellerPage = () => {
     return (
@@ -12,27 +14,46 @@ const SellerPage = () => {
                 <h2 className={styles.header}> Auction Dashboard </h2>
                 <div>
                     <ul className={styles.ul}>
-                        <li className={styles.li}>Home</li>
-                        <li className={styles.li}>Log out</li>
+                        <Link className={styles.li_link} to="">
+                            <li className={styles.li}>Home</li>
+                        </Link>
+                        <Link className={styles.li_link} to="">
+                            <li className={styles.li}>Log out</li>
+                        </Link>
+                        <li className={styles.li}>New listing</li>
                     </ul>
                 </div>
             </div>
             <div className="container">
                 <h4 className={styles.active_auction_list}>Active Auctions</h4>
                 <div className={styles.auction_body}>
-                    {data.map((auction) => {
+                    {data.map((auction, i) => {
                         return (
-                            <div className={styles.auctions}>
+                            <div key={[i]} className={styles.auctions}>
                                 <img
                                     className={styles.auction_image}
                                     src={auction.image}
                                     alt=""
                                 />
                                 <h3 className={styles.auction_title}>Title</h3>
+                                <p className={styles.price}>
+                                    Price €{auction.price}
+                                </p>
+                                <div className={styles.auction_info}>
+                                    <ul className={styles.ul}>
+                                        <li className={styles.highest_bid}>
+                                            Bid:€ {auction.currentBid}
+                                        </li>
+                                        <li className={styles.highest_bid}>
+                                            Closing: {auction.closingDate}
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         );
                     })}
                 </div>
+                <SalesHistory />
             </div>
             ;
             <FooterLayout />
