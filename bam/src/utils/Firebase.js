61393679476e6b89b715d1ac604b439c9ff519b2
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-06-13 14:02:16 +0100

    fire base evn file

--- bam/src/utils/Firebase.js
+++ bam/src/utils/Firebase.js
@@ -1,5 +1,5 @@
 // Real time data base url - https://console.firebase.google.com/u/0/project/bam-capstone/database/bam-capstone-default-rtdb/data/~2F
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAsiLTIBmDCPGnIArqsQpqBc0a8AtCdE6Y",
-    authDomain: "bam-capstone.firebaseapp.com",
-    projectId: "bam-capstone",
-    storageBucket: "bam-capstone.appspot.com",
-    messagingSenderId: "614912179148",
-    appId: "1:614912179148:web:a227950337b730df40bb69",
-    measurementId: "G-HKXWVGHBWX",
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP__PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_MEASUREMENTID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
 // Initialize Firebase
